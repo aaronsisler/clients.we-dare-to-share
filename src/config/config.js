596c335c9e34e46a28bf2e1596dded730223a6a7
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-06-04 10:36:09 -0400

    Updating for configs around location

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,20 @@
 export const CDN_URL =
   "https://cdn-e-and-b-solutions.s3.amazonaws.com/clients/we-dare-to-share";
 
+// Location
+export const CONTACT_ADDRESS = ["305 Teal Lake Dr", "Holly Springs, NC 27540"];
+export const GOOGLE_MAPS_LOCATION =
+  "https://www.google.com/maps/place/305+Teal+Lake+Dr,+Holly+Springs,+NC+27540/@35.6345787,-78.8261729";
+
 // Client Name
 export const CLIENT_NAME = "We Dare To Share";
 
 // Email
 export const POINT_OF_CONTACT_EMAIL = "REPLACE_ME__client-email@gmail.com";
 // export const POINT_OF_CONTACT_EMAIL = "aaron.sisler@eandbsolutions.com";
+
+// Phone number
+export const CONTACT_PHONE_NUMBER = "(919) 886-9101";
 
 // Services Gateway
 // export const SERVICES_GATEWAY_URL = "https://services.eandbsolutions.com/beta";
